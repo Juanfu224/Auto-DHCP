bdcfffddd6318eaa9da1bf5a43b7a550ec0b35b1
Juanfu224 <88267143+Juanfu224@users.noreply.github.com>
2023-02-10 09:11:14 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,8 +6,12 @@ sudo apt install isc-dhcp-server bind9
 wget https://github.com/webmin/webmin/releases/download/2.013/webmin_2.013_all.deb
 sudo dpkg -i webmin*
 sudo apt install -f
+sudo rm -r webmin*
 
 #HABILITAR SERVICIOS
 sudo ufw allow webmin
 sudo ufw allow isc-dhcp-server
 sudo ufw allow bind9
+
+#CONFIGURACIÓN
+sudo cp -r dhcpd.conf /etc/dhcp
